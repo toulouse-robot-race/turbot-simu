fc2cad1920d4f7744318502480ff41c3dde71078
Lior Perez <pertoul@gmail.com>
2019-06-06 23:03:19 +0200

    Fix image warper parameters

--- robot/ImageWarper.py
+++ robot/ImageWarper.py
@@ -2,8 +2,8 @@ import cv2
 
 import numpy as np
 
-T_COEFF = 1
-R_COEFF = 1
+T_COEFF = 2.3
+R_COEFF = 3
 
 
 class ImageWarper:
@@ -12,19 +12,33 @@ class ImageWarper:
         self.gyro = gyro
 
     def warp(self, image):
-        height, width = image.shape
-        bl = [0, height]
-        br = [width, height]
         rotation = self.gyro.get_delta_cap() * R_COEFF
         translation = self.tacho.get_delta_tacho() * T_COEFF
 
         # Define a tetrahedron that will become rectangular and take all screen
-        before_perspective_warp = np.float32([[100, 60], [width - 100, 60], bl, br])
-        after_perspective_warp = np.float32([[0, 0], [width, 0], bl, br])
-        perspective_matrix = cv2.getPerspectiveTransform(before_perspective_warp, after_perspective_warp)
+        h1 = 50
+        h2 = 240
+        middle = 160
+        horizon = 6
+        pente = 2
 
-        translation_matrix = np.float32([[1, 0, 0], [0, 1, translation / 2]])
-        rotation_matrix = cv2.getRotationMatrix2D((width / 2, height), rotation, 1)
+        tl = np.float32([h1, -(h1 - horizon) * pente + middle])
+        tr = np.float32([h1,  (h1 - horizon) * pente + middle])
+        bl = np.float32([h2, -(h2 - horizon) * pente + middle])
+        br = np.float32([h2,  (h2 - horizon) * pente + middle])
+        pts1 = np.array([tl, tr, bl, br])
+        # Convert to openCV coordinates system
+        pts1 = pts1[:,::-1]
+        new_height = 400
+        new_width = 300
+        height, width = new_height, new_width
+        pts2 = np.float32([[0,0],[new_width,0],[0,new_height],[new_width,new_height]])
+        perspective_matrix = cv2.getPerspectiveTransform(pts1,pts2)
+
+        translation_matrix = np.float32([[1, 0, 0], [0, 1, translation / 2 ]])
+        rotation_matrix = cv2.getRotationMatrix2D((width / 2, height), rotation , 1)
+
+        cv2.imshow("original", image)
 
         perspective = cv2.warpPerspective(image, perspective_matrix, (width, height))
         cv2.imshow("perspective", perspective)
@@ -34,6 +48,11 @@ class ImageWarper:
         cv2.imshow("rotation", rotation)
         translation2 = cv2.warpAffine(rotation, translation_matrix, (width, height))
         cv2.imshow("translation2", translation2)
+
+        perspective_matrix_inverse = cv2.getPerspectiveTransform(pts2, pts1)
+        perspective_inverse = cv2.warpPerspective(translation2, perspective_matrix_inverse, (320, 240))
+        cv2.imshow("persp corrigee", perspective_inverse)
+
         cv2.waitKey(0)
 
-        return translation2
+        return perspective_inverse
