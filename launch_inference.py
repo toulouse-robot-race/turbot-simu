c9c84e4f58d446d7fac48f60f79234f2a7efade2
Cédric Franck <siredeck@gmail.com>
2019-07-27 00:35:17 +0200

    Real car: create process to produce infered images

---
+++ launch_inference.py
@@ -0,0 +1,40 @@
+import time
+from pathlib import Path
+
+import cv2
+import numpy as np
+from keras.models import load_model
+
+MODEL_FILENAME = 'deep_learning_models/final_race_model_5_3.h5'
+
+RAM_DISK_DIR = "./"
+
+INFERENCE_DISABLE_FILE = "inference.disable"
+
+CAM_HANDLE = 1
+
+# Load model
+seq = load_model(MODEL_FILENAME)
+
+# Init camera stream
+stream = cv2.VideoCapture(CAM_HANDLE)
+stream.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+while True:
+    # Check if inference is enabled
+    if Path(INFERENCE_DISABLE_FILE).is_file():
+        time.sleep(0.1)
+        continue
+
+    _, frame = stream.read()
+
+    # Process inference
+    predicted_masks = seq.predict(frame[np.newaxis, :, :, :])[0, ...]
+    mask_line = predicted_masks[:, :, 0]
+    mask_obstacles = predicted_masks[:, :, 1]
+
+    # Save mask in ram disk files
+    np.save("mask_line", mask_line)
+    np.save("mask_obstacles", mask_obstacles)
+
