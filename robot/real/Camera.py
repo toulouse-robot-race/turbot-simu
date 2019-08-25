f3ab29172a3a2922b6af811facd0c81f60ab75f6
Cédric Franck <siredeck@gmail.com>
2019-08-25 18:39:07 +0200

    Real cam: Fix get obstacles images

--- robot/real/Camera.py
+++ robot/real/Camera.py
@@ -14,10 +14,10 @@ class Camera(Component):
 
     mask_line = None
 
-    mask_obstacle = None
+    mask_obstacles = None
 
     def execute(self):
         # Consume masks produced by inference process
         self.mask_line = np.load(self.mask_line_file_path).astype(float)
-        self.mask_obstacle = np.load(self.mask_obstacle_file_path).astype(float)
+        self.mask_obstacles = np.load(self.mask_obstacle_file_path).astype(float)
 
